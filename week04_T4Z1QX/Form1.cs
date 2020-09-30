54e7063c0f1a8eda6560d8c4ca614c32a1962b7c
András István Péceli <andras.peceli@stud.uni-corvinus.hu>
2020-09-30 17:07:18 +0200

    Flats

--- week04_T4Z1QX/Form1.cs
+++ week04_T4Z1QX/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,18 @@ namespace week04_T4Z1QX
 {
     public partial class Form1 : Form
     {
+        List<Flat> Flats;
+
+        RealEstateEntities context = new RealEstateEntities();
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        void LoadData()
+        {
+            Flats = context.Flats.ToList();
         }
     }
 }
