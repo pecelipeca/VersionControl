d83db1ff7cfbcc4d64282378582fd948285048c2
András István Péceli <andras.peceli@stud.uni-corvinus.hu>
2020-09-30 17:14:52 +0200

    Excel létrehozás

--- week04_T4Z1QX/Form1.cs
+++ week04_T4Z1QX/Form1.cs
@@ -22,11 +22,44 @@ namespace week04_T4Z1QX
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
 
         void LoadData()
         {
             Flats = context.Flats.ToList();
+        }
+
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
+        void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                xlSheet = xlWB.ActiveSheet;
+
+                //CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+
         }
     }
 }
