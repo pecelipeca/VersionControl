0709159ea3b957ab9da478432c840b704e3a6e14
András István Péceli <andras.peceli@stud.uni-corvinus.hu>
2020-10-07 15:29:24 +0200

    Portfólió tárolása memóriában

--- t4z1qx_week05/Form1.cs
+++ t4z1qx_week05/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using t4z1qx_week05.Entities;
 
 namespace t4z1qx_week05
 {
@@ -14,12 +15,24 @@ namespace t4z1qx_week05
     {
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         public Form1()
         {
             InitializeComponent();
 
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+
+            CreatePortfolio();
+        }
+
+        void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
