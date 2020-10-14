66c692e56e7d9c1665bddfa859f3eec0028a0d6b
András István Péceli <andras.peceli@stud.uni-corvinus.hu>
2020-10-14 11:34:53 +0200

    xml feldolgozás

--- week06_t4z1qx/Form1.cs
+++ week06_t4z1qx/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using week06_t4z1qx.Entities;
 using week06_t4z1qx.MnbServiceReference;
 
@@ -33,6 +34,29 @@ namespace week06_t4z1qx
             var result = response.GetExchangeRatesResult;
 
             dataGridView1.DataSource = Rates;
+
+            void xmlLoad()
+            {
+                var xml = new XmlDocument();
+                xml.LoadXml(result);
+                foreach (XmlElement x in xml.DocumentElement)
+                {
+                    var rate = new RateData();
+                    Rates.Add(rate);
+
+                    rate.Date = DateTime.Parse(x.GetAttribute("date"));
+
+                    var childElement = (XmlElement)x.ChildNodes[0];
+                    rate.Currency = childElement.GetAttribute("curr");
+
+                    var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                    var value = decimal.Parse(childElement.InnerText);
+                    if (unit != 0)
+                        rate.Value = value / unit;
+                }
+
+            }
         }
+
     }
 }
