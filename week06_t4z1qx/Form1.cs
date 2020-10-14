87f7c12984c948982fcc586101addc6a44bf3cc3
András István Péceli <andras.peceli@stud.uni-corvinus.hu>
2020-10-14 11:14:47 +0200

    webszolg. hívás

--- week06_t4z1qx/Form1.cs
+++ week06_t4z1qx/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week06_t4z1qx.MnbServiceReference;
 
 namespace week06_t4z1qx
 {
@@ -15,6 +16,19 @@ namespace week06_t4z1qx
         public Form1()
         {
             InitializeComponent();
+
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
