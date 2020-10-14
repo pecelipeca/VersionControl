4ccc660d59d95552ff84935499f9f5d85578f8ba
András István Péceli <andras.peceli@stud.uni-corvinus.hu>
2020-10-14 11:44:57 +0200

    adatok diagrammon

--- week06_t4z1qx/Form1.cs
+++ week06_t4z1qx/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using week06_t4z1qx.Entities;
 using week06_t4z1qx.MnbServiceReference;
@@ -56,6 +57,30 @@ namespace week06_t4z1qx
                 }
 
             }
+
+            xmlLoad();
+
+            chartLoad();
+            
+        }
+
+        void chartLoad()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
 
     }
