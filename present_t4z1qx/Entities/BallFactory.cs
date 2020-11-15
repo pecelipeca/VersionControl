85a31bbdefa0927c78e8d399c511dcc2d71d957e
András István Péceli <andras.peceli@stud.uni-corvinus.hu>
2020-11-15 11:32:00 +0100

    labdagyár létrehozás

---
+++ present_t4z1qx/Entities/BallFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace present_t4z1qx.Entities
+{
+    class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
