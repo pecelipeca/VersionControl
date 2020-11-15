b03ae9cfa368afb45d2c76c9f0ccf08b5bf5e443
András István Péceli <andras.peceli@stud.uni-corvinus.hu>
2020-11-15 12:15:48 +0100

    játék absztrakció kezelése

---
+++ present_t4z1qx/Abstraction/Toy.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace present_t4z1qx.Abstraction
+{
+    public abstract class Toy : Label
+    {
+        public Toy()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = 50;
+            Paint += Toy_Paint;
+        }
+
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected abstract void DrawImage(Graphics g);
+
+
+        public virtual void MoveToy()
+        {
+            Left += 1;
+        }
+    }
+}
