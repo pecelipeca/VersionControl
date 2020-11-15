65e00bc48a5a39791474ac92f5c3e2dbe79c7333
András István Péceli <andras.peceli@stud.uni-corvinus.hu>
2020-11-15 12:25:48 +0100

    gyár absztrakc kezelése

---
+++ present_t4z1qx/Abstraction/IToyFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace present_t4z1qx.Abstraction
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
