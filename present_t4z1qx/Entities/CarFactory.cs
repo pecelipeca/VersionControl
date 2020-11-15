f9f09a5756aaa25bdcd32becfc677d334875df55
András István Péceli <andras.peceli@stud.uni-corvinus.hu>
2020-11-15 12:34:38 +0100

    absztrakció felhasználása a futószalagon

--- present_t4z1qx/Entities/CarFactory.cs
+++ present_t4z1qx/Entities/CarFactory.cs
@@ -7,11 +7,11 @@ using System.Threading.Tasks;
 
 namespace present_t4z1qx.Entities
 {
-    public class BallFactory : IToyFactory
+    public class CarFactory : IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
 
         Toy IToyFactory.CreateNew()
--- present_t4z1qx/Form1.cs
+++ present_t4z1qx/Form1.cs
@@ -1,4 +1,5 @@
-﻿using present_t4z1qx.Entities;
+﻿using present_t4z1qx.Abstraction;
+using present_t4z1qx.Entities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,10 +15,10 @@ namespace present_t4z1qx
     
     public partial class Form1 : Form
     {
-        private List<Ball> _balls = new List<Ball>();
+        private List<Toy> _toys = new List<Toy>();
 
-        private BallFactory _factory;
-        public BallFactory Factory
+        private IToyFactory _factory;
+        public IToyFactory Factory
         {
             get { return _factory; }
             set { _factory = value; }
@@ -31,27 +32,27 @@ namespace present_t4z1qx
 
         private void createTimer_Tick(object sender, EventArgs e)
         {
-            var ball = Factory.CreateNew();
-            _balls.Add(ball);
-            ball.Left = -ball.Width;
-            mainPanel.Controls.Add(ball);
+            var toy = Factory.CreateNew();
+            _toys.Add(toy);
+            toy.Left = -toy.Width;
+            mainPanel.Controls.Add(toy);
         }
 
         private void conveyorTimer_Tick(object sender, EventArgs e)
         {
             var maxPosition = 0;
-            foreach (var ball in _balls)
+            foreach (var ball in _toys)
             {
-                ball.MoveBall();
+                ball.MoveToy();
                 if (ball.Left > maxPosition)
                     maxPosition = ball.Left;
             }
 
             if (maxPosition > 1000)
             {
-                var oldestBall = _balls[0];
+                var oldestBall = _toys[0];
                 mainPanel.Controls.Remove(oldestBall);
-                _balls.Remove(oldestBall);
+                _toys.Remove(oldestBall);
             }
         }
     }
