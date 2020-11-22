87cf99ea8d6801f9ec4a56158247f53bd1a68e4c
András István Péceli <andras.peceli@stud.uni-corvinus.hu>
2020-11-22 18:15:56 +0100

    jelszó validálás teszt

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnitTestExample.Controllers;
+using UnitTestExample.Entities;
 
 namespace UnitTestExample.Test
 {
@@ -22,6 +23,23 @@ namespace UnitTestExample.Test
             var accountController = new AccountController();
 
             var actualResult = accountController.ValidateEmail(email);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [
+            Test,
+            TestCase("abcdABCD", false),
+            TestCase("ABCD1234", false),
+            TestCase("abcd1234", false),
+            TestCase("Ab1234", false),
+            TestCase("Abcd1234", true)
+        ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            var accountController = new AccountController();
+
+            var actualResult = accountController.ValidatePassword(password);
 
             Assert.AreEqual(expectedResult, actualResult);
         }
